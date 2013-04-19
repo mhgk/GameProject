9607a23066b6e8dcc863e2e32ee828cdcc419d8c
mads h. g. Kverneland <madshgk@hotmail.com>
2013-04-19 15:48:33 +0200

    update 1.2 - 19/4

--- src/dk/itu/team7/game/Banana.java
+++ src/dk/itu/team7/game/Banana.java
@@ -12,9 +12,10 @@ public class Banana {
 	private int height;
 	private Color color;
 
-	private int velocity;
-	private int angle;
 	
+
+	public boolean visible;
+
 	Scanner scannerV = new Scanner(System.in);
 	Scanner scannerA = new Scanner(System.in);
 
@@ -25,36 +26,60 @@ public class Banana {
 		this.width = w;
 		this.height = h;
 		this.color = c;
+		this.visible = false;
 	}
-	
+
 	public void updateBanana(int x, int y) {
 		this.x = x;
 		this.y = y;
-		
+
 	}
 
 	public void display(Graphics g) {
 		// This method lets us draw the players
+		if (this.visible == true) {
+			// This is player rendered graphics.
+			Graphics2D graphics = (Graphics2D) g; // Graphical library to render
+													// shapes.
+			graphics.setColor(color);
+			graphics.drawRect(x, y, width, height); // draws a rectangle
+													// circumces
+			graphics.fillRect(x, y, width, height); // this fills the rectangle
+													// with
+													// colour
+			// graphics.
 
-		// This is player rendered graphics.
-		Graphics2D graphics = (Graphics2D) g; // Graphical library to render
-												// shapes.
-		graphics.setColor(color);
-		graphics.drawRect(x, y, width, height); // draws a rectangle circumces
-		graphics.fillRect(x, y, width, height); // this fills the rectangle with
-												// colour
-		// graphics.
+			projectory();
+		}
+
 	}
+
+	private double x_speed =  1;
+	private double y_speed = -2;
+    private double velocity = 0.001;
 	
-
-
-	public void projectory() {
-
-		//velocity = scannerV.nextInt();
-	//	angle = scannerA.nextInt();
-	// is this scanner suppose to be here???			
+    public void projectory() {
 		
-		x = x+1;
-		        y = y+1; 
+
+   
+		x += x_speed;
+		
+		y += y_speed;
+		
+		y_speed += velocity;
+
+		System.out.println(x + " , " + y);
+		
+		if (x < 0 || x > 1400) {
+			x = 0;
+			this.visible = false;
+			
+		}
+		
+		
+        if (y > 800) {
+            y = 0;        
+        	this.visible = false;
+        }
 	}
 }
--- src/dk/itu/team7/game/Board.java
+++ src/dk/itu/team7/game/Board.java
@@ -5,125 +5,117 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 import javax.swing.JPanel;
 
-public class Board extends JPanel implements KeyListener{
-	/** This class calls on the player class and assign values to the player parameters.
-	 * it also initialize the keyListener which takes input from the keyboard
+public class Board extends JPanel implements KeyListener {
+	/**
+	 * This class calls on the player class and assign values to the player
+	 * parameters. it also initialize the keyListener which takes input from the
+	 * keyboard
 	 */
-	
+
 	private static final long serialVersionUID = 1L;
 	Player player1;
 	Player player2;
-    Banana banana1;
-    boolean isBanana = false;
-    
+	Banana banana1;
+	boolean isBanana = false;
 
-    public Board() {
-    	// makes 2 new player objects, and assign values to the player parameters
-    	
-    	player1 = new Player(randomX()+10, 450-randomY(), 20, 40, Color.blue); //(int x, int y, int w, int h, Color c)
-    	player2 = new Player(790-randomX(), 450-randomY(), 20, 40, Color.red);
-    	banana1 = new Banana(player1.getX(), player1.getY(), 20, 20, Color.YELLOW);
-    	//banana = new Banana(x, y, w, h, Color.YELLOW);
-        
+	public Board() {
+		// makes 2 new player objects, and assign values to the player
+		// parameters
+
+		player1 = new Player(randomX() + 10, 450 - randomY(), 20, 40,
+				Color.blue); // (int x, int y, int w, int h, Color c)
+		player2 = new Player(790 - randomX(), 450 - randomY(), 20, 40,
+				Color.red);
+		banana1 = new Banana(player1.getX(), player1.getY(), 20, 20,
+				Color.YELLOW);
+		// banana = new Banana(x, y, w, h, Color.YELLOW);
+
 		addKeyListener(this);
-    }
-		
-		
-		 @Override
-    	 public void keyTyped(KeyEvent e) {}
+	}
 
-    	    @Override
-    	    public void keyPressed(KeyEvent e) {
-    	//this method calls on the method moveDown(in class player) to get the movement-range
-    	    	
-    	        int keyCode = e.getKeyCode();
-    	         
-    /*	         if (keyCode == KeyEvent.VK_S) {
-    	             player1.moveDown(); 
-    	         }
-    	         
-    	         if (keyCode == KeyEvent.VK_W) {
-    	             player1.moveUp();
-    	         }
-      	         if (keyCode == KeyEvent.VK_DOWN) {
-    	             player2.moveDown();
-    	         }
-    	         
-    	         if (keyCode == KeyEvent.VK_UP) {
-    	             player2.moveUp();
-    	         }
-    	         
-    	         */    	        
-    	        
-    	         if (keyCode == KeyEvent.VK_SPACE) {
-    	            	        	 
-    	        	 
-    	        	isBanana = true;
-    	         }     
-    	        	
-    	    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
 
-    	    @Override
-    	    public void keyReleased(KeyEvent e) {}
-    	    
-    	    
-    	    /**
-    	     * Set's focus on the panel so key events are catch. 
-    	     */
-    	    
-    	    public boolean isFocusTraversable() {
-    	        return true;
-    	   // what does this do?      
-    	        
-    	    }
-    	    
-    	    public void paint(Graphics g) {
-    	    // This method lets us paint the new objects of the players. it calls on the method Graphics g in players.
-    	    	
-    	    	player1.display(g);
-    	    	
-    	    	player2.display(g);
-    	    	
-    	    	if (isBanana) {
-    	    		for (int i = 0; i < 1200; i++) {
-    	    			banana1.projectory();
-    	    			
-    	    			banana1.display(g);
-    	    	       
-    	    		}
-    	    		
-    	    		banana1.updateBanana(player1.getX(), player1.getY());
-    	    			 
-    	    	isBanana = false;
-    	    	
-    	    	    	    		
-    	    	}
-    	    	
-    	    	
-    	    }
-    
-   public int randomX() {
-	   
-	   Random randomStartPosX = new Random();
-	   
-	   int x =  randomStartPosX.nextInt(190);
-	    
-	   return x;
-	   
-	   
-   }
-    
-   public int randomY() {
-	   
-	   Random randomStartPosY = new Random();
-	  
-	   int y = randomStartPosY.nextInt(190);
-	    
-	    return y;
-   }
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// this method calls on the method moveDown(in class player) to get the
+		// movement-range
+
+		int keyCode = e.getKeyCode();
+
+		/*
+		 * if (keyCode == KeyEvent.VK_S) { player1.moveDown(); }
+		 * 
+		 * if (keyCode == KeyEvent.VK_W) { player1.moveUp(); } if (keyCode ==
+		 * KeyEvent.VK_DOWN) { player2.moveDown(); }
+		 * 
+		 * if (keyCode == KeyEvent.VK_UP) { player2.moveUp(); }
+		 */
+
+		if (keyCode == KeyEvent.VK_SPACE) {
+
+			isBanana = true;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	/**
+	 * Set's focus on the panel so key events are catch.
+	 */
+
+	public boolean isFocusTraversable() {
+		return true;
+		// what does this do?
+
+	}
+
+	public void paint(Graphics g) {
+		// This method lets us paint the new objects of the players. it calls on
+		// the method Graphics g in players.
+		// return nothing
+		player1.display(g);
+
+		player2.display(g);
+		banana1.display(g);
+
+		if (isBanana) {
+
+			banana1.visible = true;
+			
+            banana1.updateBanana(player1.getX(), player1.getY());
+		
+
+			isBanana = false;
+
+		}
+
+	}
+
+	public int randomX() {
+
+		Random randomStartPosX = new Random();
+
+		int x = randomStartPosX.nextInt(190);
+
+		return x;
+
+	}
+
+	public int randomY() {
+
+		Random randomStartPosY = new Random();
+
+		int y = randomStartPosY.nextInt(190);
+
+		return y;
+	}
 }
-    		
